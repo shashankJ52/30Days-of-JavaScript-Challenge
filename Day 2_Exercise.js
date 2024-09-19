65fbe098a0a471193ff70c7e81654f904bc9d959
Shashank Jajimoggala <66770133+shashankJ52@users.noreply.github.com>
2024-09-20 01:36:33 +0530

    Level 2 & 3 Questions Added

--- Day 2_Exercise.js
+++ Day 2_Exercise.js
@@ -60,3 +60,49 @@ newString = newString.concat("30 Days of", "JavaScript");
 console.log(newString);
 // 25. Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log("30 Days Of JavaScript ".repeat(2));
+
+/***** Exercise LEVEL 2  *****/
+/*
+
+1.Using console.log() print out the following statement:
+
+The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+Using console.log() print out the following quote by Mother Teresa:
+
+"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+2. Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+
+3. Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+
+4. Check if 'on' is found in both python and jargon
+
+5. I hope this course is not full of jargon. Check if jargon is in the sentence.
+
+6. Generate a random number between 0 and 100 inclusively.
+
+7. Generate a random number between 50 and 100 inclusively.
+
+8. Generate a random number between 0 and 255 inclusively.
+
+9. Access the 'JavaScript' string characters using a random number.
+
+10. Use console.log() and escape characters to print the following pattern.
+
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+11. Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+*/
+/***** Exercise LEVEL 3  *****/
+/*
+'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+    const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+*/
